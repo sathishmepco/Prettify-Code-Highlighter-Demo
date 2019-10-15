df7c7906bad26cea93a52034567689765eacf704
sathishmepco <sathishmepco@gmail.com>
2019-10-15 22:56:47 +0530

    first commit

---
+++ app/src/main/java/com/cdac/androidcodehighlighter/ui/dashboard/DashboardFragment.java
@@ -0,0 +1,40 @@
+package com.cdac.androidcodehighlighter.ui.dashboard;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.cdac.androidcodehighlighter.R;
+
+public class DashboardFragment extends Fragment {
+
+    private WebView webView;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        webView = root.findViewById(R.id.webView);
+        loadCode();
+        return root;
+    }
+
+    private void loadCode(){
+        webView.getSettings().setJavaScriptEnabled(true);
+//        webView.getSettings().setLoadWithOverviewMode(true);
+//        webView.getSettings().setUseWideViewPort(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setDisplayZoomControls(false);
+        webView.setScrollContainer(false);
+        webView.loadUrl("file:///android_asset/c_demo.html");
+    }
+
+}

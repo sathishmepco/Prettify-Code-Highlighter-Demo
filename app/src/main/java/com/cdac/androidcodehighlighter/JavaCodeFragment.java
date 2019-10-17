042219c78b4d37ca3a055b8cc2fbf254fcb5ee13
sathishmepco <sathishmepco@gmail.com>
2019-10-17 23:40:20 +0530

    Android Code Highlighter

--- app/src/main/java/com/cdac/androidcodehighlighter/JavaCodeFragment.java
+++ app/src/main/java/com/cdac/androidcodehighlighter/JavaCodeFragment.java
@@ -1,27 +1,20 @@
-package com.cdac.androidcodehighlighter.ui.home;
+package com.cdac.androidcodehighlighter;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
-import android.widget.TextView;
-
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
-import com.cdac.androidcodehighlighter.R;
-
-public class HomeFragment extends Fragment {
+public class JavaCodeFragment extends Fragment {
 
     private WebView webView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        View root = inflater.inflate(R.layout.fragment_java, container, false);
         webView = root.findViewById(R.id.webView);
         loadCode();
         return root;
@@ -31,9 +24,9 @@ public class HomeFragment extends Fragment {
         webView.getSettings().setJavaScriptEnabled(true);
 //        webView.getSettings().setLoadWithOverviewMode(true);
 //        webView.getSettings().setUseWideViewPort(true);
-        webView.setScrollContainer(false);
+//        webView.setScrollContainer(false);
         webView.getSettings().setBuiltInZoomControls(true);
         webView.getSettings().setDisplayZoomControls(false);
-        webView.loadUrl("file:///android_asset/home.html");
+        webView.loadUrl("file:///android_asset/test.html");
     }
 }
--- app/src/main/java/com/cdac/androidcodehighlighter/MainActivity.java
+++ app/src/main/java/com/cdac/androidcodehighlighter/MainActivity.java
@@ -1,9 +1,7 @@
 package com.cdac.androidcodehighlighter;
 
 import android.os.Bundle;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -17,14 +15,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_java, R.id.navigation_c, R.id.navigation_python)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
     }
-
 }
